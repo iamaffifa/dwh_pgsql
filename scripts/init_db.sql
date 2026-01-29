665cd0ce2e79007d8962c82ce3f74a4679a9eab7
iamaffifa <iamaffifa@gmail.com>
2026-01-29 20:46:31 +0500

    read me file and database initialization script

---
+++ scripts/init_db.sql
@@ -0,0 +1,27 @@
+/*
+==========================================
+Creating Database and Schemas 
+==========================================
+
+Script Purpose:
+Create database ith name "dwh_pgsql" and schemas
+
+Script Warning
+None
+*/
+
+-- Creating the database
+
+CREATE DATABASE dwh_pgsql
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+
+-- Creating Schema
+CREATE SCHEMA bronze;
+CREATE SCHEMA bronze;
+CREATE SCHEMA bronze;
